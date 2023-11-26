5f39e2c1a3f83bfb01afa2265c367eadde5ff974
Franques Galindo <franquesg3@gmail.com>
2023-11-26 19:50:56 -0300

    First commit
    initial version

---
+++ scripts.js
@@ -0,0 +1,24 @@
+let selectedNote;
+
+function selectNote(note) {
+    let buttons = document.querySelectorAll(".note-button");
+    buttons.forEach(button => button.classList.remove("active"));
+    
+    document.getElementById(`button-${note}`).classList.add("active");
+    
+    selectedNote = note;
+
+    console.log(selectedNote)
+};
+
+function submitSelection() {
+    
+    console.log("função ativada.")
+    
+    document.querySelector('.rating-state').style.display = "none";
+    document.querySelector('.thank-you').style.display = "block";
+    
+    let selectedRating = document.querySelector('.selected');
+    selectedRating.innerHTML = `You selected ${selectedNote} out of 5`
+
+};
